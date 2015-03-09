dd69c7fc32b82ed660d01350e0bef4012a5cc059
German Goldenstein <german.goldenstein@gmail.com>
2015-03-09 10:34:10 -0300

    removinove gzip and add footer

--- src/frontend/middleware.clj
+++ src/frontend/middleware.clj
@@ -2,7 +2,6 @@
   (:require [taoensso.timbre :as timbre]
             [selmer.parser :as parser]
             [environ.core :refer [env]]
-            [ring.middleware.gzip :refer [wrap-gzip]]
             [selmer.middleware :refer [wrap-error-page]]
             [prone.middleware :refer [wrap-exceptions]]
             [noir-exception.core :refer [wrap-internal-error]]))
@@ -14,12 +13,10 @@
 
 (def development-middleware
   [wrap-error-page
-   wrap-exceptions
-   wrap-gzip])
+   wrap-exceptions])
 
 (def production-middleware
-  [#(wrap-internal-error % :log (fn [e] (timbre/error e)))
-   wrap-gzip])
+  [#(wrap-internal-error % :log (fn [e] (timbre/error e)))])
 
 (defn load-middleware []
   (concat (when (env :dev) development-middleware)
